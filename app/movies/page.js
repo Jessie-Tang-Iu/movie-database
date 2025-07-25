87ba944614ee4235a3476dda2329094fc1380679
Lam Dao <lam.dao@edu.sait.ca>
2025-07-24 22:53:50 -0600

    Update components

--- app/movies/page.js
+++ app/movies/page.js
@@ -1,38 +1,40 @@
 "use client";
 
-import MovieRow from "../components/MovieRow";
 import NavBar from "../components/NavBar";
 import Banner from "../components/Banner";
+import Library from "../components/Library";
+import Genre from "../components/Genre";
 
-const dummyMovies = [
-  { title: "WeCrashed", posterUrl: "wecrashed.jpg" },
-  { title: "Fallback Test", posterUrl: "nonexistent.jpg" },
-  { title: "No Image", posterUrl: "" },
-  { title: "Another One", posterUrl: "" },
-  { title: "Another Two", posterUrl: "fallback.png" },
-  { title: "Another Three", posterUrl: "" },
-  { title: "Another Four", posterUrl: "" },
-  { title: "Another Five", posterUrl: "" },
-  { title: "Another Six", posterUrl: "fakeurl.jpg" },
-  { title: "Another Seven", posterUrl: "" },
-  { title: "Another Eight", posterUrl: "" },
-  { title: "Another Nine", posterUrl: "" },
-];
+// const dummyMovies = [
+//   { title: "WeCrashed", posterWUrl: "wecrashed.jpg" },
+//   { title: "Fallback Test", posterWUrl: "nonexistent.jpg" },
+//   { title: "No Image", posterWUrl: "" },
+//   { title: "Another One", posterWUrl: "" },
+//   { title: "Another Two", posterWUrl: "fallback.png" },
+//   { title: "Another Three", posterWUrl: "" },
+//   { title: "Another Four", posterWUrl: "" },
+//   { title: "Another Five", posterWUrl: "" },
+//   { title: "Another Six", posterWUrl: "fakeurl.jpg" },
+//   { title: "Another Seven", posterWUrl: "" },
+//   { title: "Another Eight", posterWUrl: "" },
+//   { title: "Another Nine", posterWUrl: "" },
+// ];
+
+const genres = ['Action', 'Animation', 'Crime', 'Drama', 'Family', 'History', 'Music', 'Romance', 'Thriller', 'War', 'Adventure', 'Comedy', 'Documentary', 'Erotica', 'Fantasy', 'Horror', 'Mystery', 'Science fiction', 'Western'];
 
 export default function Page() {
-  return (
-    <div className="bg-black text-white min-h-screen">
-      <NavBar />
-      <Banner movies={dummyMovies} />
+    return (
+        <div className="bg-black text-white min-h-screen">
+            <NavBar />
 
-      {/* <h1 className="text-2xl font-bold px-4 pt-6">Movie Home</h1> */}
-      <MovieRow title="Boredom Busters" movies={dummyMovies} />
-      <MovieRow title="More Movies" movies={dummyMovies} />
-      <MovieRow title="More Movies" movies={dummyMovies} />
-      <MovieRow title="More Movies" movies={dummyMovies} />
-      <MovieRow title="More Movies" movies={dummyMovies} />
-      <MovieRow title="Even More Movies" movies={dummyMovies} />
-      <MovieRow title="Last Row" movies={dummyMovies} />
-    </div>
-  );
+            {/* <h1 className="text-2xl font-bold px-4 pt-6">Movie Home</h1> */}
+            <Library type="Trending" />
+            <Library type="Newest" />
+
+            {genres.map(
+                (item) => ( <Genre genre={item} /> )
+            )}
+            
+        </div>
+    );
 }
