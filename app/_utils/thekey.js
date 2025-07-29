96f10d7941164cea6fb51d487d02cf1e9d57cc2f
joywong1228 <joywong1228@gmail.com>
2025-07-29 09:52:04 -0600

    adding a test page

--- app/_utils/thekey.js
+++ app/_utils/thekey.js
@@ -1,3 +1,10 @@
 // bad idea: apiKey.js
 export const TMDB_API_KEY =
-  "1410454490cbbff0f61179b5799f41c9f08bf51029f852f5ab6071bff649f084";
+  "b8544baccd48a8f23d54da64b59ace90d7e33b1b8c3ad50fe161116bbd100b96";
+
+export const TMDB_API_KEY2 = "0892d817b06593c021f79693fbff9e78";
+
+// I have 3 keys:
+// 637397882bce97dc00d38f79c3d19c443b5edaded0448c9bc0bbebc879d1124a
+// d833f4f7f284584584d47b8fd771f05a975405f55b029bb64c8b7b0971ed70ff
+// 12200e77fe00a17c44b6b7a79d977d4e35bd3b3d77ebf9899ad60bd15af14ea6
